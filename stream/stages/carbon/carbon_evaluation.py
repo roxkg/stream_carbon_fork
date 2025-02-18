583d98ef4420e182e0aca7a282de68a2b07bd037
M.G <yuhan.sun@student.kuleuven.be>
2025-02-18 17:23:59 +0100

    modify defect density and cpa 2.18

--- stream/stages/carbon/carbon_evaluation.py
+++ stream/stages/carbon/carbon_evaluation.py
@@ -22,38 +22,47 @@ def calculate_carbon(
 ):
     #area is in unit: normalized by 1 MAC area for 1 element calculation
     total_area = scme.area_total
-    # mem_area = scme.mem_area
+    mem_area = scme.mem_area
     op_CI = scme.carbon_param.CI_op
     energy_value = float(scme.energy)
+    # pJ -> kWh
     op_co2 = op_CI * energy_value / (3.6 * 10**18)
-    lifespan = 2
-    taskspan = scme.latency/(1.8*10**9)   #clock freq = 1.8GHz
+    lifespan = scme.carbon_param.lifetime
+    frequency = scme.carbon_param.frequency
+    taskspan = scme.latency/(frequency*10**9)   
+    # year -> s 
+    # lifetime working proportion: 0.2
     lifeop_co2 = lifespan*365*24*60*60*0.2/taskspan * op_co2
-    
     
     # current technology node: 16nm -> approximated by 14nm 
     # all data from ECO-CHIP
-    defect_density = 0.09
+    #defect_density = 0.09
+    defect_density = get_defect_rate(scme.carbon_param.technology_node)
     # one large chip
-    cpa = 16.84 
-    rdl_layers = 6
+    cpa = get_carbon_per_area(scme.carbon_param.technology_node) 
+    
+    
     area = (12000/1024*total_area)*0.000008*0.000008
     print(area)
-    wastage_extra_cfp = Si_wastage_accurate_t(wafer_dia=450,chip_area=area,cpa_factors=cpa)
+    wastage_extra_cfp = waste_carbon_per_die(wafer_dia=450,chip_area=area,cpa_factors=cpa)
     yields = yield_calc(area,defect_density)
     mfg_carbon = cpa*area / yields
     mfg_carbon = mfg_carbon+wastage_extra_cfp
 
+    """
+    # below shoule be package carbon emission
+    rdl_layers = 6
     package_defect_den = defect_density/4
     pacakge_yield = yield_calc(area,package_defect_den)
-    wastage_extra_cfp = Si_wastage_accurate_t(wafer_dia=450,chip_area=area,cpa_factors=cpa)
+    wastage_extra_cfp = waste_carbon_per_die(wafer_dia=450,chip_area=area,cpa_factors=cpa)
     mfg_carbon = cpa*area / pacakge_yield
     hi_carbon = mfg_carbon+wastage_extra_cfp
-
     package_carbon = hi_carbon* 0.4916
     package_carbon *= rdl_layers/8
     cemb = package_carbon + mfg_carbon
-
+    """
+    cemb = mfg_carbon
+    
     with open(output_path, "w") as file: 
         file.write(f"op_co2 = {op_co2}g\ntotal life co2 = {lifeop_co2}\n area={total_area}\n")
         file.write(f"em_co2 = {cemb}")
@@ -64,27 +73,38 @@ def yield_calc(area, defect_density):
     yield_val = (1+(defect_density*1e4)*(area*1e-6)/10)**-10
     return yield_val
 
-def Si_wastage_accurate_t(wafer_dia,chip_area,cpa_factors):
-    si_area = (math.pi * (wafer_dia ** 2))/4
-    dpw = math.pi * wafer_dia * ((wafer_dia/(4*chip_area)) - (1/math.sqrt(2*chip_area)))
-    area_wastage = si_area - (math.floor(dpw)*chip_area)
+def waste_carbon_per_die(diameter,chip_area,cpa_factors):
+    wafer_area = (math.pi * (diameter ** 2))/4
+    dies_per_wafer = math.pi * diameter * ((diameter/(4*chip_area)) - (1/math.sqrt(2*chip_area)))
+    area_wastage = wafer_area - (math.floor(dies_per_wafer)*chip_area)
     #unused_si_cfp = wastage_cfp(area_wastage,techs,scaling_factors)
     unused_si_cfp = area_wastage*cpa_factors
-    wastage_si_cfp_pdie = unused_si_cfp/dpw
+    wastage_si_cfp_pdie = unused_si_cfp/dies_per_wafer
     return wastage_si_cfp_pdie
 
-"""
-def EMIB_carbon(
+def get_defect_rate(technology_node):
+    defect_density = open_yaml("stream/inputs/examples/carbon/defect_density.yaml")
+    nodes = open_yaml("stream/inputs/examples/carbon/technology_node.yaml")
+    # in node is in 7,10,14,22,28, return defect density directly
+    if technology_node in nodes:
+        return defect_density[nodes.index(technology_node)]
 
-): 
-    #0.33 in 16 convert to 7nm
-    router_area = 0.33/np.array([scaling_factors[ty].loc[14, 'area'] for ty in types])
-    emib_area =  [5*5]*num_if
-    #print("NUMBER OF INTERFACES",num_if)
-    emib_carbon, _, _ = Si_chip([22]*num_if, ["logic"]*num_if, emib_area, scaling_factors,transistors_per_gate,
-                        power_per_core,carbon_per_kWh,True)
-    package_carbon = np.sum(emib_carbon)* scaling_factors['beolVfeol'].loc[22,'beolVfeol']
-"""
+    # or use interpolation
+    defect_rate = np.interp(technology_node, nodes, defect_density)
+
+    return defect_rate
+
+def get_carbon_per_area(technology_node):
+    cpas = open_yaml("stream/inputs/examples/carbon/carbon_per_area.yaml")
+    nodes = open_yaml("stream/inputs/examples/carbon/technology_node.yaml")
+    # in node is in 7,10,14,22,28, return defect density directly
+    if technology_node in nodes:
+        return cpas[nodes.index(technology_node)]
+
+    # or use interpolation
+    carbon_per_area = np.interp(technology_node, nodes, cpas)
+
+    return carbon_per_area
 
 
 
